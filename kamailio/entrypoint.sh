7f694155664a9cd1cd7b52b2885320dfad5663c5
jerry <efficacy38@gmail.com>
2022-06-28 01:13:23 +0900

    add kamailio dockerfile

---
+++ kamailio/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Set default settings, pull repository, build
+# app, etc., _if_ we are not given a different
+# command.  If so, execute that command instead.
+set -e
+
+# Default values
+: ${PID_FILE:="/var/run/kamailio.pid"}
+: ${KAMAILIO_ARGS:="-DD -E -f /etc/kamailio/kamailio.cfg -P ${PID_FILE}"}
+
+# confd requires that these variables actually be exported
+export PID_FILE
+
+# Make dispatcher.list exists
+mkdir -p /data/kamailio
+touch /data/kamailio/dispatcher.list
+
+: ${PRIVATE_IPV4="$(netdiscover -field privatev4 ${PROVIDER})"}
+: ${PUBLIC_IPV4="$(netdiscover -field publicv4 ${PROVIDER})"}
+: ${PUBLIC_HOSTNAME="$(netdiscover -field hostname ${PROVIDER})"}
+
+cat <<ENDHERE >/data/kamailio/local.k
+alias=${PUBLIC_IPV4}
+listen=udp:${PRIVATE_IPV4}:5060 advertise k8s_loadbalencer_domain:5060
+listen=udp:${PRIVATE_IPV4}:5080
+ENDHERE
+
+# Runs kamaillio, while shipping stderr/stdout to logstash
+exec /usr/sbin/kamailio $KAMAILIO_ARGS $*
